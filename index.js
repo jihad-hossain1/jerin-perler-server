e2337d6d73f6eb7858fb962c4c0392109d6aaa74
jihad-hossain1 <jihadkham934@gmail.com>
2023-07-20 12:31:01 +0600

    'user'

--- index.js
+++ index.js
@@ -2,62 +2,60 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xd4auwc.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-    serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true,
-    }
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
 });
 
 async function run() {
-    try {
+  try {
+    // await client.connect();
 
-        // await client.connect();
+    // all collection here
+    const usersCollection = client.db("jerinPerler").collection("users");
 
-        // all collection here 
-        const usersCollection = client.db('jerinPerler').collection('users')
-
-        // users api 
-        app.post("/users", async (req, res) => {
-            const user = req.body;
-            console.log(user);
-            const query = { email: user.email };
-            const existingUser = await usersCollection.findOne(query);
-            if (existingUser) {
-                return res.send({ message: "user already exists" });
-            }
-            const result = await usersCollection.insertOne(user);
-            res.send(result);
-        });
-        app.get("/users", async (req, res) => {
-            const result = await usersCollection.find().toArray();
-            res.send(result);
-        });
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    } finally {
-        // Ensures that the client will close when you finish/error
-        // await client.close();
-    }
+    // users api
+    app.put("/users/:email", async (req, res) => {
+      const email = req.params.email;
+      const user = req.body;
+      const query = { email: email };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: user,
+      };
+      const result = await usersCollection.updateOne(query, updateDoc, options);
+      //   console.log(result);
+      res.send(result);
+    });
+    app.get("/users", async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    });
+    await client.db("admin").command({ ping: 1 });
+    console.log("Jerin Perler server successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
 }
 run().catch(console.dir);
 
-
 app.get("/", (req, res) => {
-    res.send("jerin Server is Running");
+  res.send("jerin Server is Running");
 });
 
 app.listen(port, () => {
-    console.log(`jerin Server Run on Port ${port}`);
+  console.log(`jerin Server Run on Port ${port}`);
 });
