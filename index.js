f991f62486eed49f4a2a824c1b8111c6fa01df5e
jihad-hossain1 <jihadkhan934@gmail.com>
2023-07-21 09:36:06 +0600

    user update delete oparation

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 5000;
 
 app.use(cors());
@@ -43,6 +43,35 @@ async function run() {
       const result = await usersCollection.find().toArray();
       res.send(result);
     });
+    app.delete("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await usersCollection.deleteOne(query);
+      res.send(result);
+    });
+    app.patch("/users/admin/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: "admin",
+        },
+      };
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+    app.put('/users/:id', async (req, res) => {
+      const id = req.params.id;
+      const item = req.body
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true }
+      const updateDoc = {
+        $set: item,
+      }
+      const result = await usersCollection.updateOne(filter, updateDoc, options)
+      console.log(result);
+      res.send(result)
+    })
     await client.db("admin").command({ ping: 1 });
     console.log("Jerin Perler server successfully connected to MongoDB!");
   } finally {
